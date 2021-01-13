6b094d73ae67e70b93948623ffdb020416ddfe69
horokos <44499944+horokos@users.noreply.github.com>
2021-01-13 16:55:45 +0100

    unittest zarys

---
+++ test.py
@@ -0,0 +1,44 @@
+import unittest
+from model import Model
+
+
+class TestModel(unittest.TestCase):
+
+    def test_grass(self):
+        pass
+
+    def test_number_of_agents(self):
+        pass
+
+    def test_get_neightbourhood(self):
+        pass
+
+    def test_move_agent(self):
+        pass
+
+    def test_remove_from_grid(self):
+        pass
+
+    def test_remove_agent(self):
+        pass
+
+    def test_collect_data(self):
+        pass
+
+
+class TestGrass(unittest.TestCase):
+
+    def test_step(self):
+        pass
+
+
+class TestPrey(unittest.TestCase):
+    pass
+
+
+class TestPredator(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
