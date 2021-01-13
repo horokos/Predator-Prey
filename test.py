564166479c9bbe86f6dbdb3bccf42ee675b2f4d7
horokos <44499944+horokos@users.noreply.github.com>
2021-01-13 17:14:54 +0100

    inny zarys unittest'ów

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from model import Model
+from agenci import Grass
 
 
 class TestModel(unittest.TestCase):
@@ -25,19 +26,16 @@ class TestModel(unittest.TestCase):
     def test_collect_data(self):
         pass
 
-
-class TestGrass(unittest.TestCase):
-
-    def test_step(self):
-        pass
-
-
-class TestPrey(unittest.TestCase):
-    pass
-
-
-class TestPredator(unittest.TestCase):
-    pass
+    def test_grass_step(self):
+        print('Running test_grass_step ...')
+        grass = Grass((0, 0), 'model')
+        grass.grown = False
+        for i in range(14):
+            grass.step()
+        self.assertFalse(grass.grown)
+        grass.step()
+        self.assertTrue(grass.grown)
+        self.assertEqual(grass.count, 15)
 
 
 if __name__ == '__main__':
